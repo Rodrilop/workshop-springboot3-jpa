fa2899548fe9c545aff6ec4d1c5c288ed0da5447
Rodrigo CS Lopes <rodrigocslopes@hotmail.com>
2023-12-30 22:24:18 -0300

    Category

--- src/main/java/com/rcslopes/course/config/TestConfig.java
+++ src/main/java/com/rcslopes/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.rcslopes.course.entities.Category;
 import com.rcslopes.course.entities.Order;
 import com.rcslopes.course.entities.User;
 import com.rcslopes.course.entities.enums.OrderStatus;
+import com.rcslopes.course.repositories.CategoryRepository;
 import com.rcslopes.course.repositories.OrderRepository;
 import com.rcslopes.course.repositories.UserRepository;
 
@@ -24,9 +26,16 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
 		
 		User u1 = new User( (long) 1, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		User u2 = new User( (long) 2, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -35,6 +44,7 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
