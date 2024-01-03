c24d96e6e264271a87b0518757a0b8e825148e2e
Rodrigo CS Lopes <rodrigocslopes@hotmail.com>
2024-01-03 00:20:04 -0300

    User update

--- src/main/java/com/rcslopes/course/resources/UserResource.java
+++ src/main/java/com/rcslopes/course/resources/UserResource.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PutExchange;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rcslopes.course.entities.User;
@@ -48,5 +50,11 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> delete(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
